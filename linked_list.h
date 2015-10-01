4664709009f430fd1f93961219b36cc5dcdc375f
Thom Groutars <tgroutars@gmail.com>
2015-10-01 09:49:14 +0200

    [IMP] Improve linked lists

--- linked_list.h
+++ linked_list.h
@@ -1,14 +1,44 @@
 #ifndef MESH_LINKEDLIST_H_
 #define MESH_LINKEDLIST_H_
 
+template <class T> class DoublyLinkedList;
+
+template <class T>
+class DoublyLinkedListElem {
+
+    T *data_;
+    DoublyLinkedListElem<T> *next_;
+    DoublyLinkedListElem<T> *previous_;
+
+  public:
+
+    // Constructors
+    DoublyLinkedListElem(T);
+    DoublyLinkedListElem(T, DoublyLinkedListElem<T>*, DoublyLinkedListElem<T>*);
+
+    // Accessors
+    DoublyLinkedListElem<T>* next() { return next_; }
+    DoublyLinkedListElem<T>* previous() { return previous_; }
+
+    // Friend class
+    friend class DoublyLinkedList<T>;
+};
+
 template <class T>
 class DoublyLinkedList {
-    T *data;
-    DoublyLinkedList *next_;
-    DoublyLinkedList *previous_;
+
+    DoublyLinkedListElem<T> *first_;
+
   public:
-    DoublyLinkedList* next() { return next_; }
-    DoublyLinkedList* previous() { return previous_; }
+
+    // Constructor / Destructor
+    DoublyLinkedList() : first_(0) { }
+    ~DoublyLinkedList();
+    void DeleteElems(DoublyLinkedListElem<T>*);
+
+    // Operations
+    void Append(T);
+    void Prepend(T);
 };
 
 #endif // MESH_LINKEDLIST_H_
